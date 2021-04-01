638472d11a877273d19cb2b0cea75cc300321991
Marharyta Mishurovska <marharyta.mishurovska@gmail.com>
2021-04-01 11:12:11 +0300

    Added menu cards wiht classes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
 
-    //Tabs
+    // Tabs
 
     const tabs = document.querySelectorAll(".tabheader__item"),
         tabsContent = document.querySelectorAll(".tabcontent"),
@@ -42,7 +42,7 @@ window.addEventListener("DOMContentLoaded", () => {
     });
 
 
-    //Timer
+    // Timer
 
 
     const deadline = '2021-05-11';
@@ -98,7 +98,7 @@ window.addEventListener("DOMContentLoaded", () => {
     setClock('.timer', deadline);
 
 
-    //Modal
+    // Modal
 
 
     const modalTrigger = document.querySelectorAll('[data-modal]'),
@@ -124,7 +124,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     modalCloseBtn.addEventListener('click', closeModal);
 
-    modal.addEventListener ('click', (event) => {
+    modal.addEventListener('click', (event) => {
         if (event.target === modal) {
             closeModal();
         }
@@ -136,16 +136,81 @@ window.addEventListener("DOMContentLoaded", () => {
         }
     });
 
-    const modalTimerId = setInterval(openModal, 5500);
+    // const modalTimerId = setInterval(openModal, 5500);
 
-    function showModalByScroll () {
+    function showModalByScroll() {
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-            openModal(); 
-            window.removeEventListener('scroll', showModalByScroll);   
-        }       
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
     }
 
     window.addEventListener('scroll', showModalByScroll);
+
+
+    // Cards and Classes
+
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parent = document.querySelector(parentSelector);
+            this.transfer = 27;
+            this.changeToUAH(); 
+        }
+
+        changeToUAH() {
+            this.price = this.price * this.transfer; 
+        }
+
+        render() {
+            const element = document.createElement('div');
+            element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+            this.parent.append(element);
+        }
+    }
+
+    new MenuCard(
+        "img/tabs/vegy.jpg",
+        "vegy",
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        9,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/post.jpg",
+        "post",
+        'Меню "Постное"',
+        'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+        14,
+        ".menu .container"
+    ).render();
+
+    new MenuCard(
+        "img/tabs/elite.jpg",
+        "elite",
+        'Меню “Премиум”',
+        'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+        21,
+        ".menu .container"
+    ).render();
 
 
 });
